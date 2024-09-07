6c4ee3381557e0ec440596eb7cd22290772c7bf1
MAHRifat <hasanrifata2@gmail.com>
2024-09-07 22:38:07 +0600

    connect online database mongodb atlas

--- app.js
+++ app.js
@@ -69,15 +69,32 @@ app.use(async (req, res, next) => {
     next();
 });
 
+// const dbUrl = process.env.ATLASDB_URL;
+
+// async function main() {
+//     await mongoose.connect(dbUrl); 
+//     // await mongoose.connect('mongodb://127.0.0.1:27017/stayAwhile'); 
+//   }
+
+
+
+// main().then(()=> {
+//     console.log("connected to DB");
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+const dbUrl = process.env.ATLASDB_URL;
+
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/stayAwhile'); 
-  }
+  await mongoose.connect(dbUrl, {
+  });
+}
 
-
-main().then(()=> {
-    console.log("connected to DB");
-}).catch((err)=>{
-    console.log(err);
+main().then(() => {
+  console.log("connected to DB");
+}).catch((err) => {
+  console.error("Connection error:", err);
 });
 
 
